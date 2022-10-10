6d4ea119227c63e002fba41db557b5ca6dc63e3a
moatazelqadi <moataz.medhat@gmail.com>
2022-10-10 17:44:07 +1100

    removed HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@
   </footer>
 </template>
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import QnA from './components/QnA.vue'
 </script>
 
